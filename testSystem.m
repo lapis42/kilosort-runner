23350cc5fb236c917e1d374eab379b4a523fde62
Dohoung <lapis42@gmail.com>
2018-09-14 14:20:52 -0400

    remove unnessasary files

--- testSystem.m
+++
@@ -1,8 +0,0 @@
-NET.addAssembly('System.Windows.Forms');
-sendkey = @(strkey) System.Windows.Forms.SendKeys.SendWait(strkey) ;
-
-system('cmd &');
-sendkey('dir')
-sendkey('{ENTER}');
-sendkey('exit')
-sendkey('{ENTER}');
