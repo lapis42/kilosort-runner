ebb41743b11a5289a0bb81a38364f43ed02eb659
Dohoung Kim <kimd11@kimd11-ws1.int.janelia.org>
2019-07-12 10:46:36 -0400

    Delete linux part

--- runPhy.m
+++ runPhy.m
@@ -2,15 +2,9 @@ function runPhy()
 %runPhy Starts Phy manual sorting
     % To use this code, phy2 should be installed using Anaconda
     % This activates 'Anaconda prompt' script and activates phy branch.
-    if strcmp(computer, 'PCWIN64') % if windows
-        startingDirectory = 'E:';
-        anacondaDirectory = ['C:\Users\', getenv('USERNAME'), '\AppData\Local\Continuum\anaconda3\'];
-    elseif strcmp(computer, 'GLNXA64') % if linux
-        startingDirectory = '/mnt/data';
-        anacondaDirectory = '~/anaconda3/bin';
-    end
+    startingDirectory = 'E:';
+    anacondaDirectory = ['C:\Users\', getenv('USERNAME'), '\AppData\Local\Continuum\anaconda3\'];
 
-    disp('==== Choose only one folder ====');
     fileList = fileSelector(startingDirectory, 1, 'params.py');
     if ~isempty(fileList) 
         filepath = fileparts(fileList{1});
@@ -18,11 +12,7 @@ function runPhy()
         return
     end    
     
-    if strcmp(computer, 'PCWIN64') % windows
-        str = [fullfile(anacondaDirectory, 'Scripts', 'activate.bat'), ' phy2 && '];
-    elseif strcmp(computer, 'GLNXA64') % linux
-        str = 'source activate phy2 && ';
-    end
+    str = [fullfile(anacondaDirectory, 'Scripts', 'activate.bat'), ' phy2 && '];
     str = [str, 'cd ', filepath, ' && phy template-gui params.py'];
 
     system(str);
